4be5f2f7006f5da54896020e53193e1a714772f1
shibukm <shibukesavmoothedath@gmail.com>
2022-02-02 16:21:08 +0530

    bankacc

---
+++ santa.py
@@ -0,0 +1,22 @@
+class bank():
+    def __init__(self,acnt,name,typ,amt):
+        self.ac=acnt
+        self.nam=name
+        self.type=typ
+        self.amount=amt
+    def printamt(self):
+        print("acnt name=",self.nam)
+        print("acntnum=",self.ac)
+        print("acnt type=",self.type)
+        print("bal=",self.amount)
+    def withl(self,wl):
+        return(self.amount-wl)
+n=input("enter name:")
+t=input("enter type:")
+a=int(input("enter number:"))
+am=int(input("enter amount:"))
+obj=bank(a,n,t,am)
+print("account details")
+obj.printamt()
+w=int(input("enter amount to withdraw:"))
+print("b1=",obj.withl(w))
