87c7fbcb3862faee5602ffaa9730019ea0f62830
shibukm <94426451+shibukm@users.noreply.github.com>
2022-03-03 11:35:53 +0530

    Model Exam

---
+++ cirFunction.py
@@ -0,0 +1,5 @@
+from math import pi
+def CirArea(r):
+    return(pi*r*r)
+def cirPerimeter(r):
+    return(2*pi*r)
---
+++ rectFunction.py
@@ -0,0 +1,4 @@
+def RectArea(l,b):
+    return(l*b)
+def RecPerimeter(l,b):
+    return(2*(l+b))
